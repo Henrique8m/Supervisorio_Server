b5810b069924505f62cf83c66b549e760cbebaa8
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-07-09 10:32:21 -0300

    Eureka Server

---
+++ S-eureka-server/src/main/java/com/hrodriguesdev/SEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.hrodriguesdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SEurekaServerApplication.class, args);
+	}
+
+}
