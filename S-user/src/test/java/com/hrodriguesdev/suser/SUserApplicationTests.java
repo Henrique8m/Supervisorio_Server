c59da5f9a8d14e46c722e0f1daaf789ed1c2819a
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-07-09 08:44:07 -0300

    Created user

---
+++ S-user/src/test/java/com/hrodriguesdev/suser/SUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrodriguesdev.suser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
